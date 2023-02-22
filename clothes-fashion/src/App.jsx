0f8e2fe582bc019e251097a393b6e7ebfb19082b
govind <govindhake39@gmail.com>
2023-02-22 22:26:02 +0530

    day-2 branch added

---
+++ clothes-fashion/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import AllRoutes from './pages/AllRoutes';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AllRoutes />
+     
+    </div>
+  );
+}
+
+export default App;
